5302b1f95034b58c3568e0ba3a36dda434e6c8f6
Aleksei Shorokhov <enslaver@list.ru>
2021-06-19 15:52:11 +0300

    добавил новый модуль spring-context-basic-crud-web-app

---
+++ spring-boot-basic-crud-web-app/src/main/java/ru/gb/springbootbasiccrudwebapp/SpringBootBasicCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.springbootbasiccrudwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicCrudWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasicCrudWebAppApplication.class, args);
+    }
+
+}
---
+++ spring-boot-basic-crud-web-app/src/test/java/ru/gb/springbootbasiccrudwebapp/SpringBootBasicCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.springbootbasiccrudwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicCrudWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
